bc58f9d509a2de8a86f5e0632355950d4a69082b
bastic18 <bastic.grant88@gmail.com>
2019-10-16 14:28:39 -0500

    added square classes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,33 @@
-window.onload= function(){
+let box; 
+let alternate = false;
+let messageBox;
 
-    var children= document.getElementById("board").children;
-    for (var i=0; i<= children.length-1; i++){
-        children[i].classList.add("square");
-    }
+window.onload= function(){ 
+    box = document.querySelector("#board");
+    box = box.querySelectorAll("div"); 
+    messageBox = document.querySelector("#status");
+    
+    box.forEach(element => {
+        element.classList.add("square");
+    });
 
 
+    box.forEach(element => {
+        element.addEventListener("click", function(){
+            if (!(element.getAttribute("class").includes("X") || element.getAttribute("class").includes("O"))) {
+                if (alternate === false) {
+                    element.className="square X";
+                    element.innerHTML = "X";
+                    alternate = true;
+                } else if (alternate === true) {
+                    element.className="square O";
+                    element.innerHTML = "O";
+                    alternate = false;
+                }
+            }
+          
+        });
+
+
+    });
 }
